6388085f9639ca4114c58c8f7481f96acfdaf793
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-09-24 18:12:09 +0200

    add /blog/category/{category} and /blog/{year} routes ass support for searching

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,6 +12,7 @@ use Strata\Data\Exception\GraphQLQueryException;
 use Strata\Data\Exception\QueryManagerException;
 use Strata\Data\Query\QueryManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,22 +30,122 @@ class BlogController extends AbstractController
      * @return Response
      * @throws GraphQLQueryException
      * @throws QueryManagerException
-     * @throws Exception
      */
     public function index(QueryManager $manager, Request $request): Response
     {
         $currentPage = $request->query->get('page', 1);
+        $search = $request->query->get('search');
         // Build queries
         $siteId = CraftCMS::getSiteForLocale($request->getLocale());
         $manager->add(
             'blogListing',
             new BlogListing(
                 $siteId,
+                null,
+                null,
+                null,
+                $search,
                 self::LIMIT,
                 $currentPage
             )
         );
+
+        return $this->buildListing($manager, $siteId, $currentPage, $search);
+    }
+
+    /**
+     * @Route("/blog/{year}", requirements={"year": "\d\d\d\d"})
+     *
+     * @param QueryManager $manager
+     * @param int          $year
+     * @param Request      $request
+     *
+     * @return Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     */
+    public function archive(QueryManager $manager, int $year, Request $request): Response
+    {
+        $currentPage = $request->query->get('page', 1);
+        $search = $request->query->get('search');
+        // Build queries
+        $siteId = CraftCMS::getSiteForLocale($request->getLocale());
+        $manager->add(
+            'blogListing',
+            new BlogListing(
+                $siteId,
+                null,
+                $year + 1,
+                $year,
+                $search,
+                self::LIMIT,
+                $currentPage
+            )
+        );
+
+        return $this->buildListing($manager, $siteId, $currentPage, $search);
+    }
+
+    /**
+     * @Route("/blog/category/{category}", requirements={"category": ".+"})
+     *
+     * @param QueryManager $manager
+     * @param string       $category
+     * @param Request      $request
+     *
+     * @return Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     */
+    public function category(QueryManager $manager, string $category, Request $request): Response
+    {
+        $currentPage = $request->query->get('page', 1);
+        $search = $request->query->get('search');
+        // Build queries
+        $siteId = CraftCMS::getSiteForLocale($request->getLocale());
+
         $manager->add('categories', new Taxonomies($siteId, 'blogCategories'));
+        $categories = $manager->getCollection('categories');
+        $categoryId = null;
+        foreach ($categories as $categoryData) {
+            if ($categoryData['slug'] == $category) {
+                $categoryId = $categoryData['id'];
+                break;
+            }
+        }
+
+        if ($categoryId == null) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        $manager->add(
+            'blogListing',
+            new BlogListing(
+                $siteId,
+                $categoryId,
+                null,
+                null,
+                $search,
+                self::LIMIT,
+                $currentPage
+            )
+        );
+
+        return $this->buildListing($manager, $siteId, $currentPage, $search);
+    }
+
+    /**
+     * @param QueryManager $manager
+     * @param int          $siteId
+     * @param int          $currentPage
+     * @param string|null  $search
+     *
+     * @return RedirectResponse|Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     */
+    protected function buildListing(QueryManager $manager, int $siteId, int $currentPage, ?string $search)
+    {
         $manager->add('filters', new BlogFilters($siteId));
 
         $collection = $manager->getCollection('blogListing');
@@ -58,28 +159,30 @@ class BlogController extends AbstractController
             return $this->redirectToRoute('app_blog_index', ['page' => 1]);
         }
 
-        $page    = $manager->get('blogListing', '[entry]');
-        $filters = $manager->getCollection('filters');
+        $page       = $manager->get('blogListing', '[entry]');
+        $categories = $manager->getCollection('filters', '[categories]');
+        $first      = $manager->get('filters', '[first]');
+        $last       = $manager->get('filters', '[last]');
 
         $archives = range(
-            (new DateTimeImmutable($filters['first']['postDate']))->format('Y'),
-            (new DateTimeImmutable($filters['last']['postDate']))->format('Y')
+            (new DateTimeImmutable($first['postDate']))->format('Y'),
+            (new DateTimeImmutable($last['postDate']))->format('Y')
         );
-        $categories = $filters['categories'];
 
         dump($archives);
         dump($page);
         dump($collection);
         dump($pagination);
-        dump($filters);
+        dump($categories);
 
         return $this->render('blog/index.html.twig', [
             'navigation' => $manager->getCollection('navigation'),
-            'page' => $page,
-            'entries' => $collection,
+            'page'       => $page,
+            'entries'    => $collection,
             'pagination' => $pagination,
             'categories' => $categories,
-            'archives' => $archives
+            'archives'   => $archives,
+            'search'     => $search
         ]);
-    }
+}
 }
