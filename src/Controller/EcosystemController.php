5a698ae4537143903ab0cea50634ab047203d9f9
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-09-29 15:26:55 +0200

    retrieve ecosystem data from craft

--- src/Controller/EcosystemController.php
+++ src/Controller/EcosystemController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
-use App\Query\CraftCMS\Page;
+use App\Query\CraftCMS\Ecosystem as CraftEcosystem;
 use App\Query\W3C\Ecosystem\Evangelists;
-use App\Query\W3C\Ecosystem\Ecosystem;
 use App\Query\W3C\Ecosystem\Groups;
 use App\Query\W3C\Ecosystem\Members;
-use App\Service\W3C;
+use Strata\Data\Exception\GraphQLQueryException;
+use Strata\Data\Exception\QueryManagerException;
 use Strata\Data\Query\QueryManager;
 use Strata\Frontend\Site;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,15 +21,18 @@ class EcosystemController extends AbstractController
      * @Route("/ecosystems/{slug}", requirements={"slug"=".+"})
      *
      * @param string       $slug
+     * @param Site         $site
      * @param QueryManager $manager
      * @param Request      $request
      *
      * @return Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
      */
     public function show(string $slug, Site $site, QueryManager $manager, Request $request): Response
     {
         // todo: retrieve proper info from craft
-        $manager->add('page', new Page($site->siteId, '/ecosystems/' . $slug));
+        $manager->add('page', new CraftEcosystem($site->siteId, 'ecosystems/' . $slug));
         $manager->add('evangelists', new Evangelists($slug));
         $manager->add('groups', new Groups($slug));
         $manager->add('members', new Members($slug));
---
+++ src/Query/CraftCMS/Ecosystem.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Query\CraftCMS;
+
+use App\Service\CraftCMS;
+use Strata\Data\Cache\CacheLifetime;
+use Strata\Data\Mapper\WildcardMappingStrategy;
+use Strata\Data\Query\GraphQLQuery;
+
+/**
+ * Get global navigation
+ */
+class Ecosystem extends GraphQLQuery
+{
+
+    public function getRequiredDataProviderClass(): string
+    {
+        return CraftCMS::class;
+    }
+
+    /**
+     * Set up query
+     *
+     * @param int $siteId Site ID of page content
+     * @param string $uri Page URI to return
+     * @param int $cacheLifetime Cache lifetime to store HTTP response for, defaults to 1 hour
+     * @throws \Strata\Data\Exception\GraphQLQueryException
+     */
+    public function __construct(int $siteId, string $uri, int $cacheLifetime = CacheLifetime::HOUR)
+    {
+        $this->setGraphQLFromFile(__DIR__ . '/graphql/ecosystem.graphql')
+            ->addFragmentFromFile(__DIR__ . '/graphql/fragments/ecosystemsFlexibleComponents.graphql')
+            ->addFragmentFromFile(__DIR__ . '/graphql/fragments/contentImage.graphql')
+            ->addFragmentFromFile(__DIR__ . '/graphql/fragments/seoData.graphql')
+            ->setRootPropertyPath('[entry]')
+            ->addVariable('uri', $uri)
+            ->addVariable('siteId', $siteId)
+            ->enableCache($cacheLifetime)
+            //->setCacheTags($uri)
+        ;
+    }
+
+    public function getMapping()
+    {
+        $mapping = new WildcardMappingStrategy();
+        $mapping->addMapping('heroIllustration', ['[heroIllustration]' => '[heroIllustration][0]']);
+
+        return $mapping;
+    }
+}
