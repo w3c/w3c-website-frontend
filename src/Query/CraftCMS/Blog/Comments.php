c429f5bdabf5807848008e9d993677edf18f7218
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-10-06 16:42:48 +0200

    comments (list and form)

---
+++ src/Query/CraftCMS/Blog/Comments.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Query\CraftCMS\Blog;
+
+use App\Service\CraftCMS;
+use Strata\Data\Cache\CacheLifetime;
+use Strata\Data\Query\GraphQLQuery;
+
+class Comments extends GraphQLQuery
+{
+
+    public function getRequiredDataProviderClass(): string
+    {
+        return CraftCMS::class;
+    }
+
+    public function __construct(int $id, int $cacheLifetime = CacheLifetime::HOUR)
+    {
+        $this->setGraphQLFromFile(__DIR__ . '/../graphql/blog/comments.graphql')
+            ->setRootPropertyPath('[comments]')
+            ->addVariable('postId', '' . $id)
+            ->enableCache($cacheLifetime)
+        ;
+    }
+}
