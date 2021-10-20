e6e9c8870276e6a8382f2cdb87bc7e806830b788
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-10-20 21:02:50 +0200

    events

---
+++ src/Controller/EventsController.php
@@ -0,0 +1,349 @@
+<?php
+
+namespace App\Controller;
+
+use App\Query\CraftCMS\Events\Entry;
+use App\Query\CraftCMS\Events\Filters;
+use App\Query\CraftCMS\Events\Listing;
+use App\Query\CraftCMS\Taxonomies\Categories;
+use App\Query\CraftCMS\YouMayAlsoLikeRelatedEntries;
+use DateTimeImmutable;
+use Exception;
+use Strata\Data\Exception\GraphQLQueryException;
+use Strata\Data\Exception\QueryManagerException;
+use Strata\Data\Query\QueryManager;
+use Strata\Frontend\Site;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * @author Jean-Guilhem Rouel <jean-gui@w3.org>
+ *
+ * @Route("/events")
+ */
+class EventsController extends AbstractController
+{
+    private const LIMIT = 10;
+
+    /**
+     * @Route("/")
+     *
+     * @param QueryManager    $manager
+     * @param Site            $site
+     * @param Request         $request
+     * @param RouterInterface $router
+     *
+     * @return Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     */
+    public function index(QueryManager $manager, Site $site, Request $request, RouterInterface $router): Response
+    {
+        $currentPage = $request->query->get('page', 1);
+        $search = $request->query->get('search');
+        
+        $manager->add(
+            'eventsListing',
+            new Listing(
+                $router,
+                $site->siteId,
+                null,
+                null,
+                null,
+                null,
+                $search,
+                self::LIMIT,
+                $currentPage
+            )
+        );
+
+        [$page, $collection, $categories, $archives] = $this->buildListing($manager, $site, $currentPage);
+        $page['breadcrumbs'] = [
+            'title'  => $page['title'],
+            'uri'    => $page['uri'],
+            'parent' => null
+        ];
+
+        return $this->render('events/index.html.twig', [
+            'site'       => $site,
+            'navigation' => $manager->getCollection('navigation'),
+            'page'       => $page,
+            'entries'    => $collection,
+            'pagination' => $collection->getPagination(),
+            'categories' => $categories,
+            'archives'   => $archives,
+            'search'     => $search
+        ]);
+    }
+
+    /**
+     * @Route("/{year}", requirements={"year": "\d\d\d\d"})
+     *
+     * @param QueryManager    $manager
+     * @param int             $year
+     * @param Site            $site
+     * @param Request         $request
+     * @param RouterInterface $router
+     *
+     * @return Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     */
+    public function archive(
+        QueryManager $manager,
+        int $year,
+        Site $site,
+        Request $request,
+        RouterInterface $router
+    ): Response {
+        $currentPage = $request->query->get('page', 1);
+        $search = $request->query->get('search');
+
+        $manager->add(
+            'eventsListing',
+            new Listing(
+
+                $router,
+                $site->siteId,
+                null,
+                null,
+                $year + 1,
+                $year,
+                $search,
+                self::LIMIT,
+                $currentPage
+            )
+        );
+
+        $singlesBreadcrumbs = $manager->get('singles-breadcrumbs');
+
+        [$page, $collection, $categories, $archives] = $this->buildListing($manager, $site, $currentPage);
+        $page['breadcrumbs'] = [
+            'title' => $year,
+            'uri' => $singlesBreadcrumbs['events']['uri'] . '/' . $year,
+            'parent' => [
+                'title'  => $singlesBreadcrumbs['events']['title'],
+                'uri'    => $singlesBreadcrumbs['events']['uri'],
+                'parent' => null
+            ]
+        ];
+        $page['title'] = $page['title'] . ' - ' . $year;
+
+        return $this->render('events/index.html.twig', [
+            'site'       => $site,
+            'navigation' => $manager->getCollection('navigation'),
+            'page'       => $page,
+            'entries'    => $collection,
+            'pagination' => $collection->getPagination(),
+            'categories' => $categories,
+            'archives'   => $archives,
+            'search'     => $search
+        ]);
+    }
+
+    /**
+     * @Route("/category/{slug}", requirements={"slug": "[^/]+"})
+     *
+     * @param QueryManager    $manager
+     * @param string          $slug
+     * @param Site            $site
+     * @param Request         $request
+     * @param RouterInterface $router
+     *
+     * @return Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     */
+    public function category(
+        QueryManager $manager,
+        string $slug,
+        Site $site,
+        Request $request,
+        RouterInterface $router
+    ): Response
+    {
+        $currentPage = $request->query->get('page', 1);
+        $search = $request->query->get('search');
+
+        $manager->add('categories', new Categories($site->siteId, 'blogCategories'));
+        $categories = $manager->getCollection('categories');
+
+        $category = [];
+        foreach ($categories as $categoryData) {
+            if ($categoryData['slug'] == $slug) {
+                $category = $categoryData;
+                break;
+            }
+        }
+
+        if ($category['id'] == null) {
+            throw $this->createNotFoundException('CategoryInfo not found');
+        }
+
+        $manager->add(
+            'eventsListing',
+            new Listing(
+                $router,
+                $site->siteId,
+                $category['id'],
+                null,
+                null,
+                null,
+                $search,
+                self::LIMIT,
+                $currentPage
+            )
+        );
+
+        [$page, $collection, $categories, $archives] = $this->buildListing($manager, $site, $currentPage);
+        $singlesBreadcrumbs = $manager->get('singles-breadcrumbs');
+
+        $page['breadcrumbs'] = [
+            'title'  => $category['title'],
+            'uri'    => $singlesBreadcrumbs['events']['uri'] . '/category/' . $slug,
+            'parent' => [
+                'title'  => $singlesBreadcrumbs['events']['title'],
+                'uri'    => $singlesBreadcrumbs['events']['uri'],
+                'parent' => null
+            ]
+        ];
+        $page['title']       = $page['title'] . ' - ' . $category['title'];
+
+        return $this->render('events/index.html.twig', [
+            'site'       => $site,
+            'navigation' => $manager->getCollection('navigation'),
+            'page'       => $page,
+            'entries'    => $collection,
+            'pagination' => $collection->getPagination(),
+            'categories' => $categories,
+            'archives'   => $archives,
+            'search'     => $search
+        ]);
+    }
+
+    /**
+     * @Route("/{type}/{year}/{slug}", requirements={"year": "\d\d\d\d"})
+     *
+     * @param string          $type
+     * @param int             $year
+     * @param string          $slug
+     * @param QueryManager    $manager
+     * @param RouterInterface $router
+     * @param Site            $site
+     * @param Request         $request
+     *
+     * @return Response
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     */
+    public function show(
+        string $type,
+        int $year,
+        string $slug,
+        QueryManager $manager,
+        RouterInterface $router,
+        Site $site,
+        Request $request
+    ): Response {
+        $manager->add('page', new Entry($site->siteId, $slug, $router));
+
+        $page = $manager->get('page');
+        if (empty($page)) {
+            throw $this->createNotFoundException('Page not found');
+        }
+
+        $postYear = intval((new DateTimeImmutable($page['postDate']))->format('Y'));
+        if ($year !== $postYear) {
+            return $this->redirectToRoute('app_events_show', ['type' => $type, 'slug' => $slug, 'year' => $postYear]);
+        }
+
+        $manager->add(
+            'crosslinks',
+            new YouMayAlsoLikeRelatedEntries($site->siteId, substr($request->getPathInfo(), 1))
+        );
+
+
+        $singlesBreadcrumbs = $manager->get('singles-breadcrumbs');
+
+        $page['seo']['expiry'] = $page['expiryDate'];
+        $page['breadcrumbs'] = [
+            'title'  => $page['title'],
+            'uri'    => $page['uri'],
+            'parent' => [
+                'title'  => $year,
+                'uri'    => $singlesBreadcrumbs['events']['uri'] . '/' . $year,
+                'parent' => [
+                    'title'  => $singlesBreadcrumbs['events']['title'],
+                    'uri'    => $singlesBreadcrumbs['events']['uri'],
+                    'parent' => null
+                ]
+            ]
+        ];
+
+        if ($this->getParameter('kernel.environment') == 'dev') {
+            dump($page);
+            dump($singlesBreadcrumbs);
+        }
+
+        // @todo use events post template
+        return $this->render('events/show.html.twig', [
+            'site'          => $site,
+            'navigation'    => $manager->getCollection('navigation'),
+            'page'          => $page,
+            'year'          => $year,
+            'slug'          => $slug,
+        ]);
+    }
+
+    /**
+     * @param QueryManager $manager
+     * @param Site         $site
+     * @param int          $currentPage
+     *
+     * @return RedirectResponse|array
+     * @throws GraphQLQueryException
+     * @throws QueryManagerException
+     * @throws Exception
+     */
+    protected function buildListing(QueryManager $manager, Site $site, int $currentPage): array
+    {
+        $manager->add('filters', new Filters($site->siteId));
+
+        $collection = $manager->getCollection('eventsListing');
+        $pagination = $collection->getPagination();
+
+        if (empty($collection) && $currentPage !== 1) {
+            return $this->redirectToRoute('app_events_index', ['page' => 1]);
+        }
+
+        if ($currentPage > $pagination->getTotalPages() && $pagination->getTotalPages() > 0) {
+            return $this->redirectToRoute('app_events_index', ['page' => 1]);
+        }
+
+        $page       = $manager->get('eventsListing', '[entry]');
+        $categories = $manager->getCollection('filters', '[categories]');
+        $first      = $manager->get('filters', '[first]');
+        $last       = $manager->get('filters', '[last]');
+
+        $archives = range(
+            (new DateTimeImmutable($first['postDate']))->format('Y'),
+            (new DateTimeImmutable($last['postDate']))->format('Y')
+        );
+
+        $page['seo']['expiry'] = $page['expiryDate'];
+
+        if ($this->getParameter('kernel.environment') == 'dev') {
+            dump($archives);
+            dump($page);
+            dump($collection);
+            dump($pagination);
+            dump($categories);
+        }
+
+        return [$page, $collection, $categories, $archives];
+    }
+}
---
+++ src/Query/CraftCMS/Events/Entry.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Query\CraftCMS\Events;
+
+use App\Service\CraftCMS;
+use Strata\Data\Cache\CacheLifetime;
+use Strata\Data\Exception\GraphQLQueryException;
+use Strata\Data\Mapper\MapArray;
+use Strata\Data\Mapper\WildcardMappingStrategy;
+use Strata\Data\Query\GraphQLQuery;
+use Strata\Data\Transform\Data\CallableData;
+use Symfony\Component\Routing\RouterInterface;
+
+class Entry extends GraphQLQuery
+{
+
+    private RouterInterface $router;
+
+    public function getRequiredDataProviderClass(): string
+    {
+        return CraftCMS::class;
+    }
+
+    /**
+     * Set up query
+     *
+     * @param int    $siteId        Site ID of page content
+     * @param string $slug
+     * @param int    $cacheLifetime Cache lifetime to store HTTP response for, defaults to 1 hour
+     *
+     * @throws GraphQLQueryException
+     */
+    public function __construct(
+        int $siteId,
+        string $slug,
+        RouterInterface $router,
+        int $cacheLifetime = CacheLifetime::HOUR
+    ) {
+        $this->router = $router;
+
+        $this->setGraphQLFromFile(__DIR__ . '/../graphql/events/entry.graphql')
+            ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/defaultFlexibleComponents.graphql')
+            ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/seoData.graphql')
+            ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/contentImage.graphql')
+            ->setRootPropertyPath('[entry]')
+
+            ->addVariable('siteId', $siteId)
+            ->addVariable('slug', $slug)
+            ->cache($cacheLifetime)
+        ;
+    }
+
+    public function getMapping()
+    {
+        $mapping = new WildcardMappingStrategy();
+        $mapping->addMapping('categories', $this->mapTaxonomy('categories', 'transformCategory'));
+        $mapping->addMapping('tags', $this->mapTaxonomy('tags', 'transformTag'));
+        $mapping->addMapping('ecosystems', $this->mapTaxonomy('ecosystems', 'transformEcosystem'));
+
+        return $mapping;
+    }
+
+    private function mapTaxonomy(string $field, string $function): array
+    {
+        return ['[' . $field . ']' => new MapArray(
+            '[' . $field . ']',
+            [
+                '[title]' => '[title]',
+                '[slug]'  => '[slug]',
+                '[uri]'   => new CallableData([$this, $function])
+            ]
+        )];
+    }
+
+    public function transformCategory(array $data): string
+    {
+        $slug = $data['slug'];
+
+        return $this->router->generate('app_blog_category', ['slug' => $slug]);
+    }
+
+    public function transformEcosystem(array $data): string
+    {
+        $slug = $data['slug'];
+        return $this->router->generate('app_ecosystem_show', ['slug' => $slug]);
+    }
+
+    public function transformTag(array $data): string
+    {
+        $slug = $data['slug'];
+
+        return $this->router->generate('app_blog_tag', ['slug' => $slug]);
+    }
+}
