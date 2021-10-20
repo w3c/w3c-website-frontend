016cbc6b5b84cb4e836f6a7b732744c63516516f
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-10-20 21:02:50 +0200

    events

---
+++ src/Query/CraftCMS/Events/Filters.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Query\CraftCMS\Events;
+
+use App\Service\CraftCMS;
+use Strata\Data\Cache\CacheLifetime;
+use Strata\Data\Exception\GraphQLQueryException;
+use Strata\Data\Mapper\MapArray;
+use Strata\Data\Query\GraphQLQuery;
+use Strata\Data\Transform\Data\CallableData;
+
+class Filters extends GraphQLQuery
+{
+
+    public function getRequiredDataProviderClass(): string
+    {
+        return CraftCMS::class;
+    }
+
+    /**
+     * Set up query
+     *
+     * @param int $siteId        Site ID of page content
+     * @param int $cacheLifetime Cache lifetime to store HTTP response for, defaults to 1 hour
+     *
+     * @throws GraphQLQueryException
+     */
+    public function __construct(int $siteId, int $cacheLifetime = CacheLifetime::HOUR)
+    {
+        $this->setGraphQLFromFile(__DIR__ . '/../graphql/events/filters.graphql')
+            ->addVariable('siteId', $siteId)
+            ->cache($cacheLifetime)
+        ;
+    }
+}
