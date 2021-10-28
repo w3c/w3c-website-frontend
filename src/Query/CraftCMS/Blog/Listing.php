3c384154cba4fe9688362afe9c3110a603762389
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-10-28 18:35:52 +0200

    post category is now single-value use urls instead of uris for taxonomies

--- src/Query/CraftCMS/Blog/Listing.php
+++ src/Query/CraftCMS/Blog/Listing.php
@@ -23,44 +23,17 @@ class Listing extends GraphQLQuery
      * Set up query
      *
      * @param int         $siteId        Site ID of page content
-     * @param int|null    $category
-     * @param int|null    $tag
-     * @param string|null $before
-     * @param string|null $after
-     * @param string|null $search
-     * @param int         $limit
-     * @param int         $page
      * @param int         $cacheLifetime Cache lifetime to store HTTP response for, defaults to 1 hour
      *
      * @throws GraphQLQueryException
      */
-    public function __construct(
-        int $siteId,
-        int $category = null,
-        int $tag = null,
-        string $before = null,
-        string $after = null,
-        string $search = null,
-        int $limit = 10,
-        int $page = 1,
-        int $cacheLifetime = CacheLifetime::HOUR
-    ) {
+    public function __construct(int $siteId, int $cacheLifetime = CacheLifetime::HOUR)
+    {
         $this->setGraphQLFromFile(__DIR__ . '/../graphql/blog/listing.graphql')
             ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/seoData.graphql')
             ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/breadcrumbs.graphql')
-            ->setRootPropertyPath('[entries]')
-            ->setTotalResults('[total]')
-            ->setResultsPerPage($limit)
-            ->setCurrentPage($page)
-
+            ->setRootPropertyPath('[entry]')
             ->addVariable('siteId', $siteId)
-            ->addVariable('category', $category)
-            ->addVariable('tag', $tag)
-            ->addVariable('before', $before)
-            ->addVariable('after', $after)
-            ->addVariable('search', $search)
-            ->addVariable('limit', $limit)
-            ->addVariable('offset', ($page - 1) * $limit)
             ->cache($cacheLifetime)
         ;
     }
