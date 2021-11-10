45ef2695ca6aba794c24dbe9b4e9d074eda8f8cb
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-11-10 18:04:10 +0100

    newsletter index and archives

---
+++ src/Query/CraftCMS/Newsletters/Collection.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Query\CraftCMS\Newsletters;
+
+use App\Service\CraftCMS;
+use Strata\Data\Cache\CacheLifetime;
+use Strata\Data\Exception\GraphQLQueryException;
+use Strata\Data\Mapper\MapArray;
+use Strata\Data\Query\GraphQLQuery;
+use Strata\Data\Transform\Data\CallableData;
+use Strata\Data\Transform\Value\DateTimeValue;
+use Strata\Data\Transform\Value\IntegerValue;
+use Symfony\Component\Routing\RouterInterface;
+
+class Collection extends GraphQLQuery
+{
+
+    private RouterInterface $router;
+
+    public function getRequiredDataProviderClass(): string
+    {
+        return CraftCMS::class;
+    }
+
+    /**
+     * Set up query
+     *
+     * @param RouterInterface $router
+     * @param int             $siteId Site ID of page content
+     * @param int|null        $year
+     * @param int             $limit
+     * @param int             $page
+     *
+     * @throws GraphQLQueryException
+     */
+    public function __construct(
+        RouterInterface $router,
+        int $siteId,
+        int $year = null,
+        int $limit = 10,
+        int $page = 1
+    ) {
+        $this->router = $router;
+
+        $this->setGraphQLFromFile(__DIR__ . '/../graphql/newsletters/collection.graphql')
+            ->setRootPropertyPath('[entries]')
+            ->setTotalResults('[total]')
+            ->setResultsPerPage($limit)
+            ->setCurrentPage($page)
+            ->addVariable('siteId', $siteId)
+            ->addVariable('limit', $limit)
+            ->addVariable('offset', ($page - 1) * $limit)
+        ;
+
+        if ($year) {
+            $this->addVariable('year', ['and', '>=' . $year, '<' . ($year+1)]);
+        }
+    }
+
+    public function getMapping()
+    {
+        return [
+            '[title]' => '[title]',
+            '[url]'   => new CallableData([$this, 'transformUrl'], '[year]', '[month]', '[day]'),
+            '[date]'  => new DateTimeValue('[date]'),
+            '[year]'  => new IntegerValue('[year]'),
+            '[month]' => new IntegerValue('[month]'),
+            '[day]'   => new IntegerValue('[day]')
+        ];
+    }
+
+    public function transformUrl(string $year, string $month, string $day): string
+    {
+        return $this->router->generate('app_newsletter_show', ['year' => $year, 'month' => $month, 'day' => $day]);
+    }
+}
---
+++ src/Query/CraftCMS/Newsletters/Filters.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Query\CraftCMS\Newsletters;
+
+use App\Service\CraftCMS;
+use Strata\Data\Cache\CacheLifetime;
+use Strata\Data\Exception\GraphQLQueryException;
+use Strata\Data\Query\GraphQLQuery;
+use Strata\Data\Transform\Data\CallableData;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class Filters extends GraphQLQuery
+{
+    private RouterInterface $router;
+    private TranslatorInterface $translator;
+
+    public function getRequiredDataProviderClass(): string
+    {
+        return CraftCMS::class;
+    }
+
+    /**
+     * Set up query
+     *
+     * @param int $siteId        Site ID of page content
+     * @param int $cacheLifetime Cache lifetime to store HTTP response for, defaults to 1 hour
+     *
+     * @throws GraphQLQueryException
+     */
+    public function __construct(
+        RouterInterface $router,
+        TranslatorInterface $translator,
+        int $siteId,
+        int $cacheLifetime = CacheLifetime::HOUR
+    ) {
+        $this->router     = $router;
+        $this->translator = $translator;
+        $this->setGraphQLFromFile(__DIR__ . '/../graphql/newsletters/filters.graphql')
+            ->addVariable('siteId', $siteId)
+            ->cache($cacheLifetime)
+        ;
+    }
+
+    public function getMapping()
+    {
+        return [
+            '[archives]' => new CallableData([$this, 'transformArchives'], '[first][year]', '[last][year]')
+        ];
+    }
+
+    public function transformArchives(string $first, string $last): array
+    {
+        $archives = [
+            [
+                'title' => $this->translator->trans(
+                    'listing.newsletters.filters.all',
+                    [],
+                    'w3c_website_templates_bundle'
+                ),
+                'url'   => $this->router->generate('app_newsletter_index')
+            ]
+        ];
+        for ($year = $first; $year <= $last; $year++) {
+            $archives[] = [
+                'title' => $year,
+                'url'   => $this->router->generate('app_newsletter_archive', ['year' => $year])
+            ];
+        }
+
+        return $archives;
+    }
+}
