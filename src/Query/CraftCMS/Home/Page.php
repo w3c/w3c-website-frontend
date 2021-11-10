e35d2dfab6bf41f3adee8700ff8e7c0602b7af25
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-11-10 12:17:41 +0100

    add url and thumbnails to featured articles

--- src/Query/CraftCMS/Home/Page.php
+++ src/Query/CraftCMS/Home/Page.php
@@ -6,11 +6,16 @@ namespace App\Query\CraftCMS\Home;
 
 use App\Service\CraftCMS;
 use Strata\Data\Exception\GraphQLQueryException;
+use Strata\Data\Mapper\MapArray;
 use Strata\Data\Mapper\WildcardMappingStrategy;
 use Strata\Data\Query\GraphQLQuery;
+use Strata\Data\Transform\Data\CallableData;
+use Symfony\Component\Routing\RouterInterface;
 
 class Page extends GraphQLQuery
 {
+    private RouterInterface $router;
+
     public function getRequiredDataProviderClass(): string
     {
         return CraftCMS::class;
@@ -19,13 +24,16 @@ class Page extends GraphQLQuery
     /**
      * Set up query
      *
-     * @param int $siteId        Site ID of page content
+     * @param RouterInterface $router
+     * @param int $siteId Site ID of page content
      *
      * @throws GraphQLQueryException
      */
-    public function __construct(int $siteId)
+    public function __construct(RouterInterface $router, int $siteId)
     {
+        $this->router = $router;
         $this->setGraphQLFromFile(__DIR__ . '/../graphql/home/page.graphql')
+             ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/thumbnailImage.graphql')
              ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/contentImage.graphql')
              ->addFragmentFromFile(__DIR__ . '/../graphql/fragments/seoData.graphql')
              ->setRootPropertyPath('[entry]')
@@ -47,7 +55,41 @@ class Page extends GraphQLQuery
             ['[workingWithIndustryCallToActionButton]' => '[workingWithIndustryCallToActionButton][0]']
         );
         $mapping->addMapping('heroIllustration', ['[heroIllustration]' => '[heroIllustration][0]']);
+        $mapping->addMapping('latestNewsFeaturedArticles', [
+            '[latestNewsFeaturedArticles]' => new MapArray('[latestNewsFeaturedArticles]', [
+                '[category]'         => '[sectionHandle]',
+                '[title]'            => '[title]',
+                '[text]'             => '[language_code]',
+                '[img]'              => '[thumbnailImage][0]',
+                '[thumbnailAltText]' => '[thumbnailAltText]',
+                '[url]'              => new CallableData(
+                    [$this, 'transformEntryUri'],
+                    '[sectionHandle]',
+                    '[slug]',
+                    '[year]'
+                )
+            ])
+        ]);
 
         return $mapping;
+    }
+
+    public function transformEntryUri(string $sectionHandle, string $slug, string $year): ?string
+    {
+        switch ($sectionHandle) {
+            case 'blogPosts':
+                $route = 'app_blog_show';
+                break;
+            case 'pressReleases':
+                $route = 'app_pressreleases_show';
+                break;
+            case 'newsArticles':
+                $route = 'app_news_show';
+                break;
+            default:
+                return null;
+        }
+
+        return $this->router->generate($route, ['slug' => $slug, 'year' => $year]);
     }
 }
