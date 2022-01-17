2e4045ba4e3a02c0b0ce6b9b333e58603df0e976
Jean-Guilhem Rouel <jean-gui@w3.org>
2022-01-17 14:29:06 +0100

    fix issue when there are no press releases

--- src/Query/CraftCMS/PressReleases/Filters.php
+++ src/Query/CraftCMS/PressReleases/Filters.php
@@ -51,8 +51,12 @@ class Filters extends GraphQLQuery
         ];
     }
 
-    public function transformArchives(string $first, string $last): array
+    public function transformArchives(string $first = null, string $last = null): array
     {
+        if (!$first) {
+            return [];
+        }
+
         $archives = [
             [
                 'title' => $this->translator->trans(
