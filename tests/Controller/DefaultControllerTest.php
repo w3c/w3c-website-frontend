494962ab34d0fc807ed8bf6369cfa58b1f5b536d
Jean-Guilhem Rouel <jean-gui@w3.org>
2024-08-30 14:55:56 +0200

    fix test suite

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -18,7 +18,7 @@ class DefaultControllerTest extends PantherTestCase
     #[DataProvider('provider')]
     public function testIndex(string $lang, string $title, string $langPrefix, string $path): void
     {
-        $crawler = $this->client->request('GET', $langPrefix . $path);
+        $this->client->request('GET', $langPrefix . $path);
 
         $this->assertSelectorAttributeContains('html', 'lang', $lang);
         $this->assertSelectorTextSame('h1', $title);
@@ -27,12 +27,35 @@ class DefaultControllerTest extends PantherTestCase
     public static function provider()
     {
         return [
-            ['lang' => 'en', 'title' => 'We believe in one web for all', 'langPrefix' => '', 'path' => '/'],
-            ['lang' => 'en', 'title' => 'Ecosystems', 'langPrefix' => '', 'path' => '/ecosystems/'],
-            ['lang' => 'en', 'title' => 'Blog listing', 'langPrefix' => '', 'path' => '/blog/'],
+            ['lang' => 'en', 'title' => 'Making the Web work', 'langPrefix' => '', 'path' => '/'],
+            ['lang' => 'en', 'title' => 'Business Ecosystems', 'langPrefix' => '', 'path' => '/ecosystems/'],
+            ['lang' => 'en', 'title' => 'Blog', 'langPrefix' => '', 'path' => '/blog/'],
             //['lang' => 'ja', 'title' => 'W3C Home', 'langPrefix' => '/ja', 'path' => '/'],
             //['lang' => 'ja', 'title' => '日本語で Landing Page', 'langPrefix' => '/ja', 'path' => '/landing-page/'],
             ['lang' => 'ja', 'title' => 'Blog listing', 'langPrefix' => '/ja', 'path' => '/blog/']
         ];
+    }
+
+    // See https://github.com/symfony/symfony/issues/53812
+    protected function restoreExceptionHandler(): void
+    {
+        while (true) {
+            $previousHandler = set_exception_handler(static fn() => null);
+
+            restore_exception_handler();
+
+            if ($previousHandler === null) {
+                break;
+            }
+
+            restore_exception_handler();
+        }
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->restoreExceptionHandler();
     }
 }
