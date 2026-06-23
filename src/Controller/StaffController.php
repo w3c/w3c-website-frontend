eaa03bdd50f99f5a05687beab3c90ac4ec7b87d3
Simon R Jones <simon@studio24.net>
2026-06-23 13:17:16 +0100

    Update DDEV to PHP 8.4, replace Annotation\Route with Attribute\Route

--- src/Controller/StaffController.php
+++ src/Controller/StaffController.php
@@ -11,7 +11,7 @@ use Strata\Data\Query\QueryManager;
 use Strata\Frontend\Site;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/staff')]
 class StaffController extends AbstractController
